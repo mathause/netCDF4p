7ea028be3291e55883a898134f995d880124b52e
Mathias Hauser <mathias.hauser@env.ethz.ch>
2015-01-09 11:45:24 +0100

    made all function & classes of netCDF4 available
    modified:   ../../__init__.py
    modified:   ../__init__.py

--- netCDF4p/__init__.py
+++ netCDF4p/__init__.py
@@ -1,1 +1,5 @@
-from .netCDF4p import Dataset, MFDataset, MSFDataset
+from .netCDF4p import Dataset, Group, MFDataset, MSFDataset, CompoundType, chartostring, stringtoarr, \
+default_encoding, _quantize, default_fillvals, MFTime, num2date, date2num, \
+date2index, __has_rename_grp__, stringtochar, chartostring, \
+getlibversion, __hdf5libversion__, __netcdf4libversion__, __version__, _StartCountStride, is_native_little, \
+_out_array_shape
